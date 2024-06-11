1bff8c679da45edd0207a679e57d046df487dc64
SS0079 <676516893@qq.com>
2024-06-11 21:52:08 +0800

    fix asset bundle bug

--- Assets/Object703/Utility/AssetLoaderManager.cs
+++ Assets/Object703/Utility/AssetLoaderManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using KittyHelpYouOut;
 using QFramework;
 using UnityEngine;
@@ -10,17 +11,24 @@ namespace Object703.Utility
         public string[] prefabNames;
         public Dictionary<string, GameObject> prefabDic = new(64);
         private ResLoader prefabLoader;
-
+        private StringBuilder sb;
+        private ushort prefabCount;
         private void Start()
         {
             prefabLoader = ResLoader.Allocate();
+            sb=new StringBuilder(100);
             for (int i = 0; i < prefabNames.Length; i++)
             {
                 var name = prefabNames[i];
                 var result = prefabLoader.LoadSync<GameObject>(name);
                 if (result==null) continue;
-                prefabDic.TryAdd(name, result);
+                var success = prefabDic.TryAdd(name, result);
+                if (success)
+                {
+                    sb.Append($"{++prefabCount}:{name}\n");
+                }
             }
+            Debug.Log(sb.ToString());
         }
     }
 }
