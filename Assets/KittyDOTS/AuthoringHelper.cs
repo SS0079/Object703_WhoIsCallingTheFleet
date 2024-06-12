9ed159225cddc6110c172d1ba4218d32cfdd1d13
SS0079 <676516893@qq.com>
2024-06-12 13:57:28 +0800

    add a authoring extension for enableable component

---
+++ Assets/KittyDOTS/AuthoringHelper.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+
+namespace KittyDOTS
+{
+    public static class AuthoringHelper
+    {
+        public static IBaker AddEnableComponent<T>(this IBaker baker,Entity target ,T component, bool enabled = true) where T : unmanaged, IComponentData , IEnableableComponent
+        {
+            baker.AddComponent(target, component);
+            baker.SetComponentEnabled<T>(target,enabled);
+            return baker;
+        }
+    }
+}
