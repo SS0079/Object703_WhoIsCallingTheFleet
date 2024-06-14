37b9915d1fc78ef871419357e76c0af0b47dc58d
SS0079 <676516893@qq.com>
2024-06-14 10:58:20 +0800

    trying to add submodule

--- Assets/KittyHelpYouOut/ServiceClass/RegexPatterns.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KittyHelpYouOut.ServiceClass
-{
-    public class RegexPatterns
-    {
-        public static string ipPattern =
-            "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
-    }
-}
