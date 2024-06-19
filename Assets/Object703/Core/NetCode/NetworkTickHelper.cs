c5e0b89b2081584cfafdc75a8fc157e68a57eb09
SS0079 <676516893@qq.com>
2024-06-19 15:19:14 +0800

    add a network tick helper for quick add network tick

---
+++ Assets/Object703/Core/NetCode/NetworkTickHelper.cs
@@ -0,0 +1,14 @@
+﻿using Unity.NetCode;
+
+namespace Object703.Core.NetCode
+{
+    public static class NetworkTickHelper
+    {
+        public static NetworkTick AddSpan(this NetworkTick now, uint span)
+        {
+            var result = now;
+            result.Add(span);
+            return result;
+        }
+    }
+}
